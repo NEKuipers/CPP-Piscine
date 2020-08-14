21c478180c747a241aacbfe5941f3ae80d01918b
Nick Kuipers <nickkuipers92@gmail.com>
2020-08-14 10:38:34 +0200

    finished day02

--- day02/ex01/Fixed.cpp
+++ day02/ex01/Fixed.cpp
@@ -11,8 +11,9 @@
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+#include <cmath>
 
-const int      _fbits = 8;
+const int      Fixed::_fbits = 8;
 
 Fixed::Fixed() {
     this->_fpval = 0;
@@ -30,19 +31,42 @@ Fixed::Fixed(const Fixed & copy) {
     std::cout << "Copy constructor called." << std::endl;
 }
 
+Fixed::Fixed(const int val) {
+    std::cout << "Int constructor called." << std::endl;
+    setRawBits(val << this->_fbits);
+    return ;
+}
+
+Fixed::Fixed(const float val) {
+    setRawBits(roundf(val * (1 << this->_fbits)));
+    std::cout << "Float constructor called." << std::endl;
+    return ;
+}
+
 Fixed   &Fixed::operator=(const Fixed & rhs) {
-    this->_fpval = rhs.getRawBits();
+    if (this != &rhs)
+        this->_fpval = rhs.getRawBits();
     std::cout << "Assignation operator called." << std::endl;
     return (*this);
 }
 
 int    Fixed::getRawBits(void) const {
-    std::cout << "getRawBits member function called." << std::endl;
     return (this->_fpval);
 }
 
 void    Fixed::setRawBits(int const raw) {
-    std::cout << "setRawBits member function called." << std::endl;
     this->_fpval = raw;
     return ;
+}
+
+float   Fixed::toFloat(void) const {
+    return ((float)getRawBits() / (1 << this->_fbits));
+}
+
+int     Fixed::toInt(void) const {
+    return (getRawBits() >> this->_fbits);
+}
+
+std::ostream            &operator<<(std::ostream & out, Fixed const & rhs) {
+    return (out << rhs.toFloat());
 }
---
+++ day02/ex01/Fixed.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   Fixed.hpp                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: nkuipers <nkuipers@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/08/06 16:21:14 by nkuipers      #+#    #+#                 */
+/*   Updated: 2020/08/06 16:44:51 by nkuipers      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+# include <iostream>
+
+class Fixed
+{
+    public:
+        Fixed();
+        ~Fixed();
+        Fixed(const Fixed & copy);
+        Fixed(const int val);
+        Fixed(const float val);
+        Fixed &     operator=(const Fixed & rhs);
+        int         getRawBits(void) const;
+        void        setRawBits(int const);
+        float       toFloat(void) const;
+        int         toInt(void) const;
+
+    private:
+            int                 _fpval;
+            static const int    _fbits;
+};
+
+std::ostream            &operator<<(std::ostream & out, Fixed const & rhs);
+
+#endif
