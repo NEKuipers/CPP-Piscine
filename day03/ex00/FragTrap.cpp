d773c7fa4e350d13a676a940412a70e90793e16c
Nick Kuipers <nickkuipers92@gmail.com>
2020-08-14 22:10:41 +0200

    update

--- day03/ex00/FragTrap.cpp
+++ day03/ex00/FragTrap.cpp
@@ -2,13 +2,49 @@
 #include "FragTrap.hpp"
 
 FragTrap::FragTrap() {
+    std::cout << "FragTrap is here!" << std::endl;
     return ;
 }
 
 FragTrap::~FragTrap() {
+    std::cout << "FragTrap is retreating!" << std::endl;
     return ;
 }
 
 FragTrap::FragTrap(std::string name) : _name(name) {
+    std::cout << "FragTrap " << name << " is here!" << std::endl;
     return ;
+}
+
+void        FragTrap::rangedAttack(std::string const & target) {
+    std::cout << "FR4G-TP " << this->_name << " attacks " << target
+        << "at range, causing " << _ranged_dmg << " points of damage!" << std::endl;
+}
+
+void        FragTrap::meleeAttack(std::string const & target) {
+
+}
+
+void        FragTrap::specialAttack(std::string const & target) {
+
+}
+
+void        FragTrap::ultraAttack(std::string const & target) {
+
+}
+
+void        FragTrap::uselessAttack(std::string const & target) {
+
+}
+
+void        FragTrap::takeDamae(std::string const & target) {
+
+}
+
+void        FragTrap::beRepaired(std::string const & target) {
+
+}
+
+void        FragTrap::vaulthunter_dot_exe(std::string const & target) {
+
 }
--- day03/ex00/FragTrap.hpp
+++ day03/ex00/FragTrap.hpp
@@ -1,6 +1,7 @@
 
 #ifndef FRAGTRAP_HPP
 # define FRAGTRAP_HPP
+# include <iostream>
 
 class FragTrap
 {
@@ -8,23 +9,28 @@ class FragTrap
         FragTrap();
         ~FragTrap();
         FragTrap(std::string name);
-
-
+        FragTrap &  operator=(FragTrap const & rhs);
+        void        rangedAttack(std::string const & target);
+        void        meleeAttack(std::string const & target);
+        void        specialAttack(std::string const & target);
+        void        ultraAttack(std::string const & target);
+        void        uselessAttack(std::string const & target);
+        void        takeDamage(std::string const & target);
+        void        beRepaired(std::string const & target);
+        void        vaulthunter_dot_exe(std::string const & target);
 
     private:
-        std::string     name;
+        std::string     _name;
         unsigned int    _hp;
         unsigned int    _maxhp;
         unsigned int    _ep;
         unsigned int    _maxep;
         unsigned int    _lv;
-        unsigned int    _melee_dps;
-        unsigned int    _ranged_dps;
+        unsigned int    _melee_dmg;
+        unsigned int    _ranged_dmg;
         unsigned int    _armor_dmg_red;
-    
 
-
-}
+};
 
 
 #endif
