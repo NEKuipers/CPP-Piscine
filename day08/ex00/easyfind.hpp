a144bd20f1e9ef6546353e624d5d6a39f8fc9995
Nick Kuipers <nkuipers@f1r5s20.codam.nl>
2020-09-17 11:55:05 +0200

    fix

--- day08/ex00/easyfind.hpp
+++ day08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: nkuipers <nkuipers@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/09/04 17:30:24 by nkuipers      #+#    #+#                 */
-/*   Updated: 2020/09/17 10:43:15 by nkuipers      ########   odam.nl         */
+/*   Updated: 2020/09/17 11:54:53 by nkuipers      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void    easyFind(T &container, int n)
     i = std::find(container.begin(), container.end(), n);
     std::cout << "Searching for number " << n << "..." << std::endl;
     if (i != container.end())
-        std::cout << "Value was found in the container at memory address " << &i << "." << std::endl;
+        std::cout << "Value was found in the container." << std::endl;
     else
         throw (std::string("Value was not found in container."));
 }
